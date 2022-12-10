a583202b8ad28a6801e18788347123ad52127910
LauraSaparkhan <120064402+LauraSaparkhan@users.noreply.github.com>
2022-12-10 09:12:47 +0600

    Add files via upload

---
+++ Новая папка/GithubTask/Questions.java
@@ -0,0 +1,11 @@
+public class Questions {
+    public static void main(String[] args) {
+        System.out.println("Name: Laura");
+        System.out.println("Surname: Saparkhan");
+        System.out.println("Id: 220103028");
+        System.out.println("Hobby: Reading");
+        System.out.println("How much do you like ICT(from 1-10)): 8");
+        System.out.println("Rate this task from 1 to 10: 8");
+        System.out.println("My favorite color?: Blue");
+    }
+}
